ff9b47cad36dc1761d5c2277f0348949989faf13
Grigory Pomadchin <gr.pomadchin@gmail.com>
2016-08-15 22:24:49 +0300

    update readme docs

--- .docker/run-server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker exec -it geotrellischattademo_geodocker-spark-master_1 bash -c ". ~/.bashrc; cd /data/geotrellis-chatta-demo/geotrellis && java -cp target/scala-2.10/GeoTrellis-Tutorial-Project-assembly-0.1-SNAPSHOT.jar geotrellis.chatta.Main"
